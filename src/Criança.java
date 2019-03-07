5ce01f4a0172ca288e36bcfd12e86c17edaccc37
BarroMar <mateus.a98@outlook.com>
2019-03-07 20:30:44 -0300

    iniciando programação a objeto

---
+++ src/Criança.java
@@ -0,0 +1,18 @@
+    class Criança {
+
+        String corCabelo;
+        String corPele;
+        double altura;
+        int idade;
+
+        //atributos
+        
+            void brincar() {
+                System.out.println(" Brincndo... ");
+
+            }
+
+            void correr() {
+                System.out.println(" Correndo... ");
+            }
+    }
