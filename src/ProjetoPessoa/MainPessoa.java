f1a6a251c547e57b85289405c60196459dfead22
BarroMar <mateus.a98@outlook.com>
2019-03-28 20:01:26 -0300

    exercicio do medico construtor 28/03/2019

---
+++ src/ProjetoPessoa/MainPessoa.java
@@ -0,0 +1,7 @@
+package ProjetoPessoa;
+
+public class MainPessoa {
+    public static void main(String args[]) {
+        Pessoa p1 = new Pessoa("Pedro", "23455334");
+    }
+}
---
+++ src/ProjetoPessoa/Pessoa.java
@@ -0,0 +1,12 @@
+package ProjetoPessoa;
+
+public class Pessoa {
+    String nome;
+    String cpf;
+
+    //construtor
+    Pessoa(String nome, String cpf){
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+}
