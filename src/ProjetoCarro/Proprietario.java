f6138fc2006577917656102b9322d47d9f1f9287
BarroMar <mateus.a98@outlook.com>
2019-04-04 19:10:39 -0300

    getter and setter

--- src/ProjetoCarro/Proprietario.java
+++ src/ProjetoCarro/Proprietario.java
@@ -4,6 +4,87 @@ public class Proprietario {
     String nome;
     String cpf;
     String rg;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getDatadeNascimento() {
+        return datadeNascimento;
+    }
+
+    public void setDatadeNascimento(String datadeNascimento) {
+        this.datadeNascimento = datadeNascimento;
+    }
+
+    public String getRua() {
+        return Rua;
+    }
+
+    public void setRua(String rua) {
+        Rua = rua;
+    }
+
+    public String getBairro() {
+        return Bairro;
+    }
+
+    public void setBairro(String bairro) {
+        Bairro = bairro;
+    }
+
+    public String getCidade() {
+        return Cidade;
+    }
+
+    public void setCidade(String cidade) {
+        Cidade = cidade;
+    }
+
+    public String getEstado() {
+        return Estado;
+    }
+
+    public void setEstado(String estado) {
+        Estado = estado;
+    }
+
+    public String getCep() {
+        return Cep;
+    }
+
+    public void setCep(String cep) {
+        Cep = cep;
+    }
+
+    public String getComplemento() {
+        return Complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        Complemento = complemento;
+    }
+
     String datadeNascimento;
     String Rua;
     String Bairro;
